a435861090e076389ae660ddd8253bbb71c0f5d3
Mikko Perkele <25417912+perkmi@users.noreply.github.com>
2025-01-31 16:43:21 -0600

    added csharp_ls

--- pc/.config/nvim/lua/lsp.lua
+++ pc/.config/nvim/lua/lsp.lua
@@ -2,13 +2,14 @@
 -- look in https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require("mason-lspconfig").setup {
-    ensure_installed = { "clangd", "hydra_lsp", "hyprls", "jsonls", "lua_ls", "rust_analyzer", "taplo", "vimls", "yamlls" }
+    ensure_installed = { "clangd", "csharp_ls", "hydra_lsp", "hyprls", "jsonls", "lua_ls", "rust_analyzer", "taplo", "vimls", "yamlls" }
 }
 require'lspconfig'.clangd.setup{
   capabilities = capabilities,
   cmd = {"clangd"},
   filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
 }
+require'lspconfig'.clangd.setup{}
 require'lspconfig'.hydra_lsp.setup{}
 require'lspconfig'.hyprls.setup{}
 require'lspconfig'.jsonls.setup{}
