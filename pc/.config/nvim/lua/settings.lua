23ca784728cbc722d4f9186b5491f349e48f5bb5
Mikko Perkele <25417912+perkmi@users.noreply.github.com>
2025-03-18 01:09:01 -0500

    updated hyrpland config

--- pc/.config/nvim/lua/settings.lua
+++ pc/.config/nvim/lua/settings.lua
@@ -13,15 +13,3 @@ vim.o.splitbelow = true
 vim.o.clipboard = "unnamedplus"
 
 vim.g.rustfmt_autosave = 1
-
-vim.g.clipboard = {
-    name = 'OSC 52',
-    copy = {
-        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-    },
-    paste = {
-        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-    },
-}
