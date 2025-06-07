7740568d93baf47f4e46d3686eb042e07eaf9db1
Mikko Perkele <25417912+perkmi@users.noreply.github.com>
2025-06-07 03:05:44 -0500

    changed wiki colors

--- gentoo/.config/nvim/lua/plugins/misc.lua
+++ gentoo/.config/nvim/lua/plugins/misc.lua
@@ -17,12 +17,12 @@ return {
         'vimwiki/vimwiki',
 
         config = function()
-            vim.api.nvim_set_hl(0, "VimwikiHeader1", {fg = "#87d7ff"})
-            vim.api.nvim_set_hl(0, "VimwikiHeader2", {fg = "#327E9F"})
-            vim.api.nvim_set_hl(0, "VimwikiHeader3", {fg = "#327E9F"})
-            vim.api.nvim_set_hl(0, "VimwikiHeader4", {fg = "#327E9F"})
-            vim.api.nvim_set_hl(0, "VimwikiHeader5", {fg = "#327E9F"})
-            vim.api.nvim_set_hl(0, "VimwikiHeader6", {fg = "#327E9F"})
+            vim.api.nvim_set_hl(0, "VimwikiHeader1", {fg = "#569CD6"})
+            vim.api.nvim_set_hl(0, "VimwikiHeader2", {fg = "#9CDCFE"})
+            vim.api.nvim_set_hl(0, "VimwikiHeader3", {fg = "#608B4E"})
+            vim.api.nvim_set_hl(0, "VimwikiHeader4", {fg = "#DCDCAA"})
+            vim.api.nvim_set_hl(0, "VimwikiHeader5", {fg = "#CE9178"})
+            vim.api.nvim_set_hl(0, "VimwikiHeader6", {fg = "#F44747"})
         end
     },
     {
